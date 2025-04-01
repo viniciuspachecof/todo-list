14abd40594d3c7efd51b667a5ea4a03b55667b93
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-03-31 23:09:26 -0300

    Header, logo e variaveis globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import './global.css';
+import { PlusCircle } from '@phosphor-icons/react'
+
+import styles from './App.module.css';
+
+
+import { Header } from './components/Header';
+
 function App() {
   return (
     <>
-      <p>início</p>      
+      <Header />
+
+      <form className={styles.formTarefas} action="">
+        <input type="text" placeholder="Adicione uma nova tarefa" />
+        <button>
+          <span>Teste</span>
+          <PlusCircle size={20} />
+          </button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import todoLogo from '../assets/todo-logo.svg';
+
+import styles from './Header.module.css';
+
+export function Header() {
+  return (  
+    <header className={styles.header}>
+      <img src={todoLogo} alt="logotipo Todo List" />
+    </header>
+  );
+}
