9e7f2675388ba9be9a6dccdb1002041f54ba6503
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-04-01 23:08:14 -0300

    estilização projeto lista tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './global.css';
-import { PlusCircle } from '@phosphor-icons/react'
+
+import { PlusCircle } from '@phosphor-icons/react';
 
 import styles from './App.module.css';
-
+import clipBoard from './assets/clipboard.svg';
 
 import { Header } from './components/Header';
+import { Tarefas } from './components/Tarefas';
 
 function App() {
   return (
@@ -14,10 +16,30 @@ function App() {
       <form className={styles.formTarefas} action="">
         <input type="text" placeholder="Adicione uma nova tarefa" />
         <button>
-          <span>Teste</span>
+          <span>Criar</span>
           <PlusCircle size={20} />
-          </button>
+        </button>
       </form>
+
+      <div className={styles.tarefas}>
+        <div className={styles.registrosTarefas}>
+          <p className={styles.textoTarefas}>
+            Tarefas criadas <span>0</span>
+          </p>
+          <p className={styles.textoConcluidos}>
+            Concluídas <span>0</span>
+          </p>
+        </div>
+        <div className={styles.listaTarefas}>
+          <div className={styles.tarefasVazio}>
+            <img src={clipBoard} alt="clipboard" />
+            <p>Você ainda não tem tarefas cadastradas</p>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+
+          <Tarefas />   
+        </div>
+      </div>
     </>
   );
 }
